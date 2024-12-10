2576484d4914d964797a04288f812f1184989bb8
ilamarre <isabellelamarre62@gmail.com>
2024-12-10 14:21:32 +0100

    routesputusersetdogs

--- routes/dogs.js
+++ routes/dogs.js
@@ -9,29 +9,44 @@ router.post('/', (req, res) => {
     if (!checkBody(req.body, ['name', 'sex'])) {
         res.json({ result: false, error: 'Missing or empty fields' });
         return;
-      }
-    //Dog.findOne().then(data => {
-        const newdog = new Dog({
-            name: req.body.name,
-            sex: req.body.sex,
-            race: req.body.race,
-            birthday: req.body.birthday,
-            status: req.body.status,
-            chipid: req.body.chipid,
-            isTaken: false,
-            isFake:false,
-        });
-       
-        newdog.save().then(() => {
-            res.json({ result: true , data: newdog})
-            console.log(Dog)
-        })
-        //   else {
-        // res.json({ result: false, error: "veuillez saisir le nom" })
+    }
+    const newdog = new Dog({
+        name: req.body.name,
+        sex: req.body.sex,
+        race: req.body.race,
+        birthday: req.body.birthday,
+        status: req.body.status,
+        chipid: req.body.chipid,
+        isTaken: false,
+        isFake: false,
+    });
+
+    newdog.save().then(() => {
+        res.json({ result: true, data: newdog })
+        console.log(Dog)
     })
-//});
 
+})
 
+router.put('/updatedog/:_id', (req, res) => {
+    id=req.params.id
+    Dog.findOne({id}).then(data=>{
+        console.log(data)
+        data.name=req.body.name;
+        data.photo=req.body.photo;
+        data.sex=req.body.sex;
+        data.race=req.body.race;
+        data.birthday=req.body.birthday;
+        data.status=req.body.status;
+        data.chipid=req.body.chipid;
+        data.isTaken=req.body.isTaken;
+    
+        data.save().then(dataupdated => {
+            res.json({ result: true, message: "modifications effectuées", data: dataupdated })
+          })
+    })
+
+})
 
 
 module.exports = router;
