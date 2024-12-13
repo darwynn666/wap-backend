38be4270e93280667e12c88370caab2950b4c04f
Fabrice Guichard <fabrice.guichard@gmail.com>
2024-12-13 17:30:44 +0100

    tdd generate users

--- generate_fake_users.test.js
+++ generate_fake_users.test.js
@@ -1,19 +1,33 @@
-const request = require('supertest');
-const app = require('./app');
-const mongoose = require('mongoose');
-const User = require('./models/users')
+const request = require("supertest");
+const app = require("./app");
+const mongoose = require("mongoose");
+const User = require("./models/users");
 
-beforeEach (async () =>await User.findOne())
-it('POST /generatedata/users', async () => {
- const res = await request(app).post('/generatedata/users');
- expect(res.statusCode).toBe(200);
+beforeEach(async () => await User.findOne());
+it("POST /generatedata/users", async () => {
+  const res = await request(app).post("/generatedata/users");
+  expect(res.statusCode).toBe(200);
 });
 
-it('POST /generatedata/users', async () => {
-    const res = await request(app).post('/generatedata/users');
-    expect(res.body.result).toBe("test");  
-   });
+it("POST /generatedata/users", async () => {
+  const res = await request(app).post("/generatedata/users");
+  expect(res.body.result).toBe(false);
+});
 
-afterAll( async() => {mongoose.connection.close()
-    done()
-})
+it("POST /generatedata/users?nbUser", async () => {
+  const res = await request(app).post("/generatedata/users?nbUser");
+  expect(res.body.result).toBe(true);
+  expect(res.body.data.length).toBe(10);
+  User.deleteMany()
+});
+
+it("POST /generatedata/users?nbUser=10&longitude=50.6&latitude=3&longitudeDelta=0.05&latitudeDelta=0.05", async () => {
+    const res = await request(app).post("/generatedata/users?nbUser=10&longitude=50.6&latitude=3&longitudeDelta=0.05&latitudeDelta=0.05");
+    expect(res.body.result).toBe(true);
+    expect(res.body.data.length).toBe(10);
+    User.deleteMany()
+  });
+
+afterAll(async () => {
+  mongoose.connection.close();
+});
